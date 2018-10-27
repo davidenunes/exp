4f30504de26e6921732901215ae13ac11852d689
Davide Nunes <davidelnunes@gmail.com>
2018-10-27 19:02:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ from distutils.core import setup
 setup(name='exp',
       version='0.3',
       description='Python tool do design and run experiments with global optimisation and grid search',
+      licence='apache-2.0',
       author='Davide Nunes',
-      author_email='davide@davidenunes.com',
+      author_email='mail@davidenunes.com',
+      url = 'https://github.com/davidenunes/exp',
       packages=['exp'],
       install_requires=[
           'toml',

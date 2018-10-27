bb69252bcace0de17baefeccd2c8b6a3ed06a7d0
Davide Nunes <davidelnunes@gmail.com>
2018-10-27 19:11:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@
 
 from distutils.core import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='exp',
       version='0.3',
       description='Python tool do design and run experiments with global optimisation and grid search',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       licence='apache-2.0',
       author='Davide Nunes',
       author_email='mail@davidenunes.com',
